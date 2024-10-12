627da07230a721ebfa935f88854aed5d22c4c77b
Ridwan4D <ridwanul161@gmail.com>
2024-10-12 21:59:35 +0600

    data getting for add new store

--- index.js
+++ index.js
@@ -195,12 +195,7 @@ async function run() {
       res.send(result)
     })
 
-    app.delete("/stores/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
-      const result = await storeCollection.deleteOne(query);
-      res.send(result)
-    })
+    
     // ========================================   store collection end    ========================================
 
 
