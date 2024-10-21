bdc339dd119b00bdfc6c572bf5325023546598e3
Ridwan4D <ridwanul161@gmail.com>
2024-10-21 16:43:10 +0600

    sequence maintaind

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express()
 
 // middle ware 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174','https://spectacular-stardust-c34d34.netlify.app'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'https://spectacular-stardust-c34d34.netlify.app'],
   credentials: true,
   optionsSuccessStatus: 200
 };
@@ -56,7 +56,7 @@ async function run() {
 
     app.get('/products/:id', async (req, res) => {
       const id = req.params.id
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
       const result = await productCollection.findOne(query);
       res.send(result);
     })
@@ -384,20 +384,6 @@ async function run() {
       const result = await sliderCollection.insertOne(bannerInfo);
       res.send(result);
     })
-    // ========================================   reviews start     ========================================
-
- app.post('/reviews',async(req,res)=>{
-  const reviewData = req.body;
-  const result = await reviewCollection.insertOne(reviewData)
-  res.send(result)
- })
-
-//  get review data 
-app.get('/review',async(req,res)=>{
-  const result = await reviewCollection.find().toArray();
-  res.send(result)
-})
-
 
     app.delete('/banners/:id', async (req, res) => {
       const id = req.params.id;
@@ -405,6 +391,19 @@ app.get('/review',async(req,res)=>{
       const result = await sliderCollection.deleteOne(query);
       res.send(result);
     })
+    // ========================================   reviews start     ========================================
+    app.post('/reviews', async (req, res) => {
+      const reviewData = req.body;
+      const result = await reviewCollection.insertOne(reviewData)
+      res.send(result)
+    })
+
+    //  get review data 
+    app.get('/review', async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result)
+    })
+
     // ========================================   slider collection end    ========================================
 
 
