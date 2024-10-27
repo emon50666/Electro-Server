b2fcbe1da23a0f4dafb2d7aef841274062a121f0
Emon <emon50666@gmail.com>
2024-10-27 09:02:38 +0600

    payment api create

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ async function run() {
     const storeCollection = client.db('ElectroMart').collection('stores')
     const promotionCollection = client.db('ElectroMart').collection('promotions')
     const reviewCollection = client.db('ElectroMart').collection('reviews')
-    const checkoutCollection = client.db('ElectroMart').collection('checkout')
+    const checkoutCollection = client.db('ElectroMart').collection('order')
     const sliderCollection = client.db('ElectroMart').collection('sliders')
     const rightTopSliderCollection = client.db('ElectroMart').collection('rightTopSliders')
     const rightBottomLCollection = client.db('ElectroMart').collection('rightBottomLSliders')
@@ -479,7 +479,7 @@ async function run() {
 
     // ========================================   Checkout page api    ========================================
 
- app.post('/checkout',async(req,res)=>{
+ app.post('/order',async(req,res)=>{
   const formData = req.body;
   const result = await checkoutCollection.insertOne(formData)
   res.send(result)
@@ -494,7 +494,53 @@ async function run() {
     })
 
 
+    // ========================================   Payment method api SSL E-commerce   ========================================
+      // create na new id every api call 
+      const tran_id = new ObjectId().toString()
+    app.post('/order',async(req,res)=>{
+      // calculation product price 
+ 
+      const product = await productCollection.findOne({_id: new ObjectId(req.body.productId) })
+      console.log(product);
 
+      const data = {
+        total_amount: 100,
+        currency: 'BDT',
+        tran_id: tran_id, // use unique tran_id for each api call
+        success_url: 'http://localhost:3030/success',
+        fail_url: 'http://localhost:3030/fail',
+        cancel_url: 'http://localhost:3030/cancel',
+        ipn_url: 'http://localhost:3030/ipn',
+        shipping_method: 'Courier',
+        product_name: 'Computer.',
+        product_category: 'Electronic',
+        product_profile: 'general',
+        cus_name: 'Customer Name',
+        cus_email: 'customer@example.com',
+        cus_add1: 'Dhaka',
+        cus_add2: 'Dhaka',
+        cus_city: 'Dhaka',
+        cus_state: 'Dhaka',
+        cus_postcode: '1000',
+        cus_country: 'Bangladesh',
+        cus_phone: '01711111111',
+        cus_fax: '01711111111',
+        ship_name: 'Customer Name',
+        ship_add1: 'Dhaka',
+        ship_add2: 'Dhaka',
+        ship_city: 'Dhaka',
+        ship_state: 'Dhaka',
+        ship_postcode: 1000,
+        ship_country: 'Bangladesh',
+    };
+    // const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+    // sslcz.init(data).then(apiResponse => {
+    //     // Redirect the user to payment gateway
+    //     let GatewayPageURL = apiResponse.GatewayPageURL
+    //     res.redirect(GatewayPageURL)
+    //     console.log('Redirecting to: ', GatewayPageURL)
+    // });
+    })
 
 
 
